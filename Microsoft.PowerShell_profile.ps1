9f92f1c392400720f74026f8ac92aa0284373a70
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-16 09:39:05 +0700

    Profile configs!

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,30 @@
+# Profile Configs
+
+function parseConfigs ($path) {
+  if (-not(test-path $path)) {
+    return;
+  }
+
+  $configs = @{};
+  $content = get-content $path;
+
+  foreach ($line in $content) {
+    $trimmedLine = $line.trim();
+
+    if ($trimmedLine.startsWith(";") -or $trimmedLine -eq "") {
+      continue;
+    }
+
+    $config, $configValue = $trimmedLine -split ":";
+    $configs[$config.toUpper()] = $configValue.toUpper()
+  }
+
+  return $configs;
+}
+
+$configsPath = "$HOME/profile-configs.txt";
+$configs = parseConfigs($configsPath);
+
 # Paths
 
 $paths = @(
