051ea28d25d2986732f8c15d0927c6a2ce4a8200
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-11 17:12:34 +0700

    4 spaces for tabs

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,35 +1,35 @@
 # Core Functions
 
 function shouldIgnoreLine ($line) {
-  return $trimmedLine.startsWith(";") -or $trimmedLine -eq "";
+    return $trimmedLine.startsWith(";") -or $trimmedLine -eq "";
 }
 
 function isDirectory ($path) {
-  return (get-item $path).PSIsContainer;
+    return (get-item $path).PSIsContainer;
 }
 
 # Profile Configs
 
 function parseConfigs ($path) {
-  if (-not(test-path $path)) {
-    return;
-  }
-
-  $configs = @{};
-  $content = get-content $path;
-
-  foreach ($line in $content) {
-    $trimmedLine = $line.trim();
-
-    if (shouldIgnoreLine($trimmedLine)) {
-      continue;
+    if (-not(test-path $path)) {
+        return;
     }
 
-    $config, $configValue = $trimmedLine -split ":=";
-    $configs[$config.toUpper()] = $configValue.toUpper()
-  }
+    $configs = @{};
+    $content = get-content $path;
 
-  return $configs;
+    foreach ($line in $content) {
+        $trimmedLine = $line.trim();
+
+        if (shouldIgnoreLine($trimmedLine)) {
+            continue;
+        }
+
+        $config, $configValue = $trimmedLine -split ":=";
+        $configs[$config.toUpper()] = $configValue.toUpper()
+    }
+
+    return $configs;
 }
 
 $configsPath = "$HOME/profile/configs.txt";
@@ -38,91 +38,91 @@ $configs = parseConfigs($configsPath);
 # Environment Variables
 
 function parseEnvVars ($path) {
-  if (-not(test-path $path)) {
-    return;
-  }
-
-  $envVars = @{};
-  $content = get-content $path;
-
-  foreach ($line in $content) {
-    $trimmedLine = $line.trim();
-
-    if (shouldIgnoreLine($trimmedLine)) {
-      continue;
+    if (-not(test-path $path)) {
+        return;
     }
 
-    $envVar, $envVarValue = $trimmedLine -split ":=";
-    $envVars[$envVar] = $envVarValue;
-  }
+    $envVars = @{};
+    $content = get-content $path;
 
-  return $envVars;
+    foreach ($line in $content) {
+        $trimmedLine = $line.trim();
+
+        if (shouldIgnoreLine($trimmedLine)) {
+            continue;
+        }
+
+        $envVar, $envVarValue = $trimmedLine -split ":=";
+        $envVars[$envVar] = $envVarValue;
+    }
+
+    return $envVars;
 }
 
 $envVarsPath = "$HOME/profile/env-vars.txt";
 $envVars = parseEnvVars($envVarsPath);
 
 if ($envVars.count -gt 0) {
-  $envVars.GetEnumerator() | Foreach-Object {
-    $path = join-path "env:" $_.name;
-    set-item $path $_.value;
-  }
+    $envVars.GetEnumerator() | Foreach-Object {
+        $path = join-path "env:" $_.name;
+        set-item $path $_.value;
+    }
 }
 
 # Paths
 
 function parsePaths ($path) {
-  if (-not(test-path $path)) {
-    return;
-  }
-
-  $paths = [System.Collections.ArrayList]@();
-  $content = get-content $path;
-
-  foreach ($line in $content) {
-    $trimmedLine = $line.trim();
-
-    if (shouldIgnoreLine($trimmedLine)) {
-      continue;
+    if (-not(test-path $path)) {
+        return;
     }
 
-    $paths.add($trimmedLine);
-  }
+    $paths = [System.Collections.ArrayList]@();
+    $content = get-content $path;
 
-  return $paths;
+    foreach ($line in $content) {
+        $trimmedLine = $line.trim();
+
+        if (shouldIgnoreLine($trimmedLine)) {
+            continue;
+        }
+
+        $paths.add($trimmedLine);
+    }
+
+    return $paths;
 }
 
 $pathsPath = "$HOME/profile/paths.txt";
 $paths = parsePaths($pathsPath);
 
 foreach ($path in $paths) {
-  $env:path = "$env:path;$path";
+    $env:path = "$env:path;$path";
 }
 
 # Bookmarks
 
 function parseBookmarks ($path) {
-  if (-not(test-path $path)) {
-    return;
-  }
-
-  $bookmarks = @{};
-  $content = get-content $path;
-
-  foreach ($line in $content) {
-    $trimmedLine = $line.trim();
-
-    if (shouldIgnoreLine($trimmedLine)) {
-      continue;
+    if (-not(test-path $path)) {
+        return;
     }
 
-    $bookmark, $bookmarkValue = $trimmedLine -split ":=";
-    $bookmarkValue = $bookmarkValue.replace("`$HOME", $HOME); # magic
+    $bookmarks = @{};
+    $content = get-content $path;
 
-    $bookmarks[$bookmark] = $bookmarkValue;
-  }
+    foreach ($line in $content) {
+        $trimmedLine = $line.trim();
 
-  return $bookmarks;
+        if (shouldIgnoreLine($trimmedLine)) {
+            continue;
+        }
+
+        $bookmark, $bookmarkValue = $trimmedLine -split ":=";
+        $bookmarkValue = $bookmarkValue.replace("`$HOME", $HOME); # magic
+
+        $bookmarks[$bookmark] = $bookmarkValue;
+    }
+
+    return $bookmarks;
 }
 
 $bookmarksPath = "$HOME/profile/bookmarks.txt";
@@ -132,108 +132,108 @@ $b = $bookmarks;
 # Modules
 
 function parseModules ($path) {
-  if (-not (test-path $path)) {
-    return;
-  }
-
-  $modules = [System.Collections.ArrayList]@();
-  $content = get-content $path;
-
-  foreach ($line in $content) {
-    $trimmedLine = $line.trim();
-
-    if (shouldIgnoreLine($trimmedLine)) {
-      continue;
+    if (-not (test-path $path)) {
+        return;
     }
 
-    $modules.add($trimmedLine);
-  }
+    $modules = [System.Collections.ArrayList]@();
+    $content = get-content $path;
 
-  return $modules;
+    foreach ($line in $content) {
+        $trimmedLine = $line.trim();
+
+        if (shouldIgnoreLine($trimmedLine)) {
+            continue;
+        }
+
+        $modules.add($trimmedLine);
+    }
+
+    return $modules;
 }
 
 $modulesPath = "$HOME/profile/modules.txt";
 $modules = parseModules($modulesPath);
 
 foreach ($module in $modules) {
-  if ($module -eq 0) {
-    continue;
-  }
+    if ($module -eq 0) {
+        continue;
+    }
 
-  Import-Module ($module);
+    Import-Module ($module);
 }
 
 # Aliases;
 
 function parseAliases ($path) {
-  if (-not (test-path $path)) {
-    return;
-  }
-
-  $aliases = @{};
-  $content = get-content $path;
-
-  foreach ($line in $content) {
-    $trimmedLine = $line.trim();
-
-    if (shouldIgnoreLine($trimmedLine)) {
-      continue;
+    if (-not (test-path $path)) {
+        return;
     }
 
-    $alias, $aliasValue = $line -split ":=";
-    $aliases[$alias] = $aliasValue;
-  }
+    $aliases = @{};
+    $content = get-content $path;
 
-  return $aliases;
+    foreach ($line in $content) {
+        $trimmedLine = $line.trim();
+
+        if (shouldIgnoreLine($trimmedLine)) {
+            continue;
+        }
+
+        $alias, $aliasValue = $line -split ":=";
+        $aliases[$alias] = $aliasValue;
+    }
+
+    return $aliases;
 }
 
 $aliasesPath = "$HOME/profile/aliases.txt";
 $aliases = parseAliases($aliasesPath);
 
 if ($aliases.count -gt 0) {
-  $aliases.GetEnumerator() | Foreach-Object {
-    Set-Alias -name $_.name -Value $_.value;
-  }
+    $aliases.GetEnumerator() | Foreach-Object {
+        Set-Alias -name $_.name -Value $_.value;
+    }
 }
 
 # Functions
 
 function editor ($path) {
-  if ($configs.USENVIM -eq "TRUE") {
-    nvim $path;
-    return;
-  }
+    if ($configs.USENVIM -eq "TRUE") {
+        nvim $path;
+        return;
+    }
 
-  vim $path;
+    vim $path;
 }
 
 function v {
-  param (
-    [string]$path = ".",
-    [boolean]$shouldChangeWorkingDirectory = $true
-  )
+    param (
+        [string]$path = ".",
+        [boolean]$shouldChangeWorkingDirectory = $true
+    )
 
-  $isDirectory = isDirectory($path);
+    $isDirectory = isDirectory($path);
 
-  if (-not($isDirectory)) {
-    editor $path;
-    return;
-  }
+    if (-not($isDirectory)) {
+        editor $path;
+        return;
+    }
 
-  if ($isDirectory -and $shouldChangeWorkingDirectory) {
-    cd $path;
-  }
+    if ($isDirectory -and $shouldChangeWorkingDirectory) {
+        cd $path;
+    }
 
-  editor .;
+    editor .;
 }
 
 function runAutohotkey ($argsToPass) {
-  Invoke-Expression "$($configs.AUTOHOTKEYEXE) $argsToPass";
+    Invoke-Expression "$($configs.AUTOHOTKEYEXE) $argsToPass";
 }
 
 function bugn () {
-  if (-not($configs.BUGNDIRECTORY)) {
-    $warning = @"
+    if (-not($configs.BUGNDIRECTORY)) {
+        $warning = @"
 
 Please configure BUGNDIRECTORY at `$HOME\profile-configs.txt to use this command
 
@@ -246,48 +246,48 @@ bugnDirectory:=C:\bugn
 
 "@
 
-    echo $warning;
+        echo $warning;
 
-    return;
-  }
+        return;
+    }
 
-  $oldPath = (pwd).path;
+    $oldPath = (pwd).path;
 
-  cd (Join-Path -Path $configs.BUGNDIRECTORY -ChildPath "src");
-  runAutohotkey Main.ahk;
+    cd (Join-Path -Path $configs.BUGNDIRECTORY -ChildPath "src");
+    runAutohotkey Main.ahk;
 
-  cd $oldPath;
+    cd $oldPath;
 }
 
 function mkcd ($directory) {
-  mkdir $directory;
-  cd $directory;
+    mkdir $directory;
+    cd $directory;
 }
 
 function fp {
-  param (
-    [switch]$v = $false
-  )
+    param (
+        [switch]$v = $false
+    )
 
-  $directory = ls -n "~/projects" | fzf;
+    $directory = ls -n "~/projects" | fzf;
 
-  if (-not($directory)) {
-    return;
-  }
+    if (-not($directory)) {
+        return;
+    }
 
-  cd "~/projects/$directory";
+    cd "~/projects/$directory";
 
-  if (-not($v)) {
-    return;
-  }
+    if (-not($v)) {
+        return;
+    }
 
-  v .;
+    v .;
 }
 
 function fpv {
-  fz -v;
+    fz -v;
 }
 
 function .. {
-  cd ..;
+    cd ..;
 }
